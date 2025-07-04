d8c1e2484337ecc40edec988da8ade957df5ffd4
Marwen MAMLOUK <marwen.mamlouk@esprit.tn>
2025-07-04 15:22:19 +0200

    Correct SecurityConfig and resolve SonarQube hotspots

--- leave-management-service/src/main/java/company/Leave_Management_service/DTO/UserDTO.java
+++ leave-management-service/src/main/java/company/Leave_Management_service/DTO/UserDTO.java
@@ -18,12 +18,9 @@ public class UserDTO {
     private Long managerId;
     private String position;
 
-    public UserDTO(String user123, String s, Object o, Object o1, Object o2) {
-    }
 
-    public UserDTO() {
 
-    }
+
 
     public enum UserRole {
         EMPLOYEE, MANAGER, HR, ADMIN, SUPER_ADMIN
